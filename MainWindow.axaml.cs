47e06c098cfb0ead70980f7b9ddb925dcff787b1
jridder <jridder.neusta.de>
2024-11-08 11:17:43 +0100

    feat: show list of appointments in MainWindow

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using avalonia_terminplaner.ViewModels;
 using Avalonia.Controls;
 
 namespace avalonia_terminplaner;
@@ -5,5 +6,6 @@ namespace avalonia_terminplaner;
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
     }
 }
