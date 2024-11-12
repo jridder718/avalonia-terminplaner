93cbd0edd1aba0d29b2322abc5f6c42999ba382c
Maxim Bitz <maxim.b8@schule.bremen.de>
2024-11-12 10:56:33 +0100

    Delete & Add Methoden bereitgestellt, sowie Styling angepasst

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,47 @@
 using avalonia_terminplaner.ViewModels;
 using Avalonia.Controls;
+using System;
+using Avalonia.Interactivity;
+using avalonia_terminplaner.Models;
 
 namespace avalonia_terminplaner;
 
 public partial class MainWindow : Window {
+    private readonly MainWindowViewModel _viewModel;
+
     public MainWindow() {
         InitializeComponent();
-        DataContext = new MainWindowViewModel();
+        _viewModel = new MainWindowViewModel();
+        DataContext = _viewModel;
+
+        AddAppointment("Kundenbesuch im Office", "Bitte denkt daran, einen Parkplatz freizuhalten, damit der Kunde sich drauf stellen kann.", DateTime.Now, TimeSpan.FromDays(1));
+        AddAppointment("Arbeitszeitbetrug", "Nicht arbeiten", DateTime.Now, TimeSpan.FromDays(1));
+    }
+
+    private void Add_Appointment_Button_Click(object? sender, RoutedEventArgs e)
+    {
+        AddAppointment("Titel", "Beschreibung", DateTime.Now, TimeSpan.FromDays(1));
+    }
+
+    private void AddAppointment(string title, string description, DateTime date, TimeSpan time)
+    {
+        _viewModel.Appointments.Add(new()
+        {
+            Title = title,
+            Description = description,
+            Date = date,
+            Time = time
+        });
+    }
+
+    private void Delete_Appointment_Button_Click(object? sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var appointment = button?.DataContext as Appointment;
+
+        if (appointment != null)
+        {
+            _viewModel.Appointments.Remove(appointment);
+        }
     }
 }
